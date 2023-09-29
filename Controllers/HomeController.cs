aaeaf20895bcbc40276385c35da9e1ceea37182d
Ronaldo <Ronaldo@DESKTOP-VP14HUH>
2023-09-28 23:26:40 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Trabajo1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MiPerfil()
+        {
+            return View();
+        }
+    }
+}
